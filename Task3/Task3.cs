7fe461cb3cf3dcc8967c181643ad5bc2b15cc4bc
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-12-11 23:13:36 +0300

    Task3

---
+++ Task3/Task3.cs
@@ -0,0 +1,36 @@
+﻿namespace Task3
+{
+    public class Task3
+    {
+        // Задача 3: Задайте произвольный массив.
+        // Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+        public static void Main (string [] args){
+            int size = 10;
+            int min = 1;
+            int max = 9;
+            int [] array = CreateArray(size, min, max);
+            PrintArray(array);
+            System.Console.WriteLine(TurnAroundArray(array, size - 1));
+        }
+        public static string TurnAroundArray (int [] array, int index){
+            if (index == 0) return $"{array[index]}";
+            return $"{array[index]} " + TurnAroundArray(array, index - 1);
+        }
+        public static int [] CreateArray (int size, int min, int max){
+            Random rand = new Random();
+            int [] array = new int [size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(min, max + 1);
+            }
+            return array;
+        }
+        public static void PrintArray (int [] array){
+            foreach (int item in array)
+            {
+                System.Console.Write(item + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
